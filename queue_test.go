fdd181cf93abbc52e1471f9d7aa9daa98cfcb70f
Changkun Ou <hi@changkun.de>
2026-06-06 12:30:46 +0200

    all: update copyright year to 2026 for new code

--- queue_test.go
+++ queue_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 The golang.design Initiative authors.
+// Copyright 2026 The golang.design Initiative authors.
 // All rights reserved. Use of this source code is governed
 // by a MIT license that can be found in the LICENSE file.
 
